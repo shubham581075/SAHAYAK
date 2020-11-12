bd1e271f6ba59ee6dbff43dc4a844d5b4ae157ed
shubham581075 <45900545+shubham581075@users.noreply.github.com>
2020-11-12 22:18:34 +0530

    Revert "changed sqlite & media"

--- core/urls.py
+++ core/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('contact/',views.contact,name='Contact'),
     path('services/<serv>/',views.userservice,name='Userservice'),
     path('services/<serv>/<submit_serv>/',views.submitservice,name='Submitservice'),
-    ##path('sendmsg/',views.sendmsg,name='Sendmsg')
+    path('sendmsg/',views.sendmsg,name='Sendmsg')
 ]
